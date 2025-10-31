d91ed4b29769787769d9cf1cff7224bbca8ab7f6
Dsnehith28 <snehithdamuluri@gmail.com>
2025-10-31 15:36:25 +0530

    Add files via upload

---
+++ GradeCalculator.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+class GradeCalculator 
+{
+    public static void main(String args[])
+    {
+       Scanner s=new Scanner(System.in);
+       System.out.println("Enter Number Of Subjects:");
+       int n=s.nextInt();
+       double totalmarks=0;
+       for(int i=0;i<n;i++)
+       {
+        System.out.println("Enter marks for subject "+(i+1)+":");
+        double marks=s.nextDouble();
+        totalmarks+=marks;
+       }
+       System.out.println("Total marks:"+totalmarks);
+       double average=totalmarks/n;
+       System.out.println("Average marks:"+average);
+         if(average>=90)
+         {
+          System.out.println("Grade:A");
+         }
+         else if(average>=80)
+         {
+          System.out.println("Grade:B");
+         }
+         else if(average>=70)
+         {
+          System.out.println("Grade:C");
+         }
+         else if(average>=60)
+         {
+          System.out.println("Grade:D");
+         }
+         else
+         {
+          System.out.println("Grade:F");
+         }
+         s.close();
+    }
+}
